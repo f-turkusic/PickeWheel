b5727d4f29ec2bec1f44efe3af9bfd7c7dc5b2a3
faruk-turkusic <Faruk.turkusic@olympiatech.com>
2023-11-26 15:00:34 +0100

    add more aligment
    add more aligment

--- script.js
+++ script.js
@@ -29,6 +29,18 @@ var data = [
     { "label": "HONDA", "value": 4, "question": "What CSS property is used for changing the boldness of text?" }, //font-weight
     { "label": "FERRARI", "value": 5, "question": "What CSS property is used for changing the size of text?" }, //font-size
     { "label": "APARTMENT", "value": 6, "question": "What CSS property is used for changing the background color of a box?" }, //background-color
+    { "label": "Faruk", "value": 7, "question": "What CSS property is used for specifying the area between the content and its border?" }, // padding
+    { "label": "Alma", "value": 8, "question": "What CSS property is used for changing the font?" }, //font-family
+    { "label": "SUZUKI", "value": 9, "question": "What CSS property is used for changing the color of text?" }, //color
+    { "label": "HONDA", "value": 10, "question": "What CSS property is used for changing the boldness of text?" }, //font-weight
+    { "label": "FERRARI", "value": 11, "question": "What CSS property is used for changing the size of text?" }, //font-size
+    { "label": "APARTMENT", "value": 12, "question": "What CSS property is used for changing the background color of a box?" }, //background-color
+    { "label": "Faruk", "value": 1, "question": "What CSS property is used for specifying the area between the content and its border?" }, // padding
+    { "label": "Alma", "value": 2, "question": "What CSS property is used for changing the font?" }, //font-family
+    { "label": "SUZUKI", "value": 3, "question": "What CSS property is used for changing the color of text?" }, //color
+    { "label": "HONDA", "value": 4, "question": "What CSS property is used for changing the boldness of text?" }, //font-weight
+    { "label": "FERRARI", "value": 5, "question": "What CSS property is used for changing the size of text?" }, //font-size
+    { "label": "APARTMENT", "value": 6, "question": "What CSS property is used for changing the background color of a box?" }, //background-color
 
 ];
 
